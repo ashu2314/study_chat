7bc3495fab65b9fbe3b4bfb7d811a64dc833845a
ashutosh.l.singh <ashutosh.l.singh@accenture.com>
2025-03-01 17:57:13 +1100

    Added login page

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from openai import OpenAI
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    user_id = Column(String)
+    user_password = Column(String)
+    
+connection_string = f"mysql+pymysql://{st.secrets['DB_USER']}:{st.secrets['DB_PASSWORD']}@sql12.freesqldatabase.com:3306/sql12765026"
 
 st.set_page_config(page_title="Streamlit Chat")
 st.title("Chatbot")
+
+engine = create_engine(connection_string)
+Session = sessionmaker(bind=engine)
 
 if "setup_complete" not in st.session_state:
     st.session_state["setup_complete"] = False
@@ -11,12 +28,77 @@ if "user_message_count" not in st.session_state:
     st.session_state.user_message_count = 0
 if "messages" not in st.session_state:
         st.session_state.messages = []
+if "show_login" not in st.session_state:
+    st.session_state["show_login"] = True
+if "login_success" not in st.session_state:
+    st.session_state["login_success"] = False
     
 def complete_setup():
     if st.session_state["name"]:
         st.session_state.setup_complete = True
+def show_login_form(is_login: bool):
+    st.session_state.show_login = is_login
+
+def login(userId, password):
+        userIdLower = userId.lower()
+        with Session() as session:
+            try:
+                result = session.query(User).filter(User.user_id == userIdLower).all()
+                for user in result:
+                    if user.user_password == password:
+                        st.session_state.name = user.name
+                        st.session_state.login_success = True
+                        
+                if st.session_state.login_success:
+                    st.write("Successfully logged!!")
+                else:
+                    st.write("Incorrect userId/password!!")
+            except Exception as e:
+                st.error(f"Error occurred: {e}")
+            finally:
+                session.close()
+            
+def register(name, userId, password):
+        userIdLower = userId.lower()
+        with Session() as session:
+            try:
+                result = session.query(User).filter(User.user_id == userIdLower).all()
+                if len(result) > 0:
+                    st.write(f"User Id '{userId}' already registered!!!")
+                    show_login_form(True)
+                else:
+                    user = User(name = name, 
+                                user_id = userIdLower,
+                                user_password = password
+                            )
+                    session.add(user)
+                    session.commit()
+                    st.write('Registered successfully!!!')
+                    show_login_form(True)
+                    
+            except Exception as e:
+                st.error(f"Error occurred: {e}")
+            finally:
+                session.close()
+
+if not st.session_state.login_success and st.session_state.show_login:
+    st.title("Login with userId")
+    userId = st.text_input(label = "User Id", key = 'userid_login', max_chars = 40, placeholder = "Enter your user id")
+    password = st.text_input(label = "Password", key = 'pass_login', type = "password", max_chars = 40, placeholder = "Enter your password")
+
+    st.button("Login", key = 'login', on_click=login, args=(userId, password), disabled=(userId == '' or password == ''))
+    st.button("Signup", key='showSignup', on_click=show_login_form, args=(False,))
     
-if not st.session_state.setup_complete:
+elif not st.session_state.login_success and not st.session_state.show_login:
+    st.title("Register")
+    name = st.text_input(label = "Name", key = 'name_register', max_chars = 40, placeholder = "Enter your name")
+    userId = st.text_input(label = "User Id", key = 'userid_register', max_chars = 40, placeholder = "Enter your user id")
+    password = st.text_input(label = "Password", key = 'pass_register', type = "password", max_chars = 40, placeholder = "Enter your password")
+    st.button("Signup", key = 'signup', on_click=register, args=(name, userId, password), disabled=(userId == '' or password == '' or name == ''))
+    st.button("Login", key='showLogin', on_click=show_login_form, args=(True,)) 
+    
+if not st.session_state.setup_complete \
+    and st.session_state.login_success:
 
     st.subheader('Personal information', divider='rainbow')
     
@@ -33,7 +115,8 @@ if not st.session_state.setup_complete:
         options=["First", "Second", "Third", "Fourth", "Fifth",
             "Sixth", "Seventh", "Eighth", "Ninth", "Tenth",
             "Eleventh", "Twelfth"],
-        placeholder="Choose one"
+        placeholder="Choose one",
+        index=5
     )
     st.session_state["subject"] = st.selectbox(
         "Choose your subject",
@@ -69,7 +152,7 @@ if st.session_state.setup_complete:
             "content": (f"You are a subject matter expert of {st.session_state['subject']} "
                         f"for {st.session_state['standard']} standard and answers the questions precisely "
                         f"for the student with name {st.session_state['name']}."
-                        f"Respond as bullet points")
+                        f"Respond as bullet points in not more than 5")
             }]
 
     for message in st.session_state.messages:
